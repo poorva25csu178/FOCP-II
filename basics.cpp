cccfd1e14b9c4e4d6d44b0790da9f67a35fedbdb
poorva25csu178 <poorva25csu178@ncuindia.edu>
2026-01-20 11:08:59 +0530

    basics

---
+++ basics.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+/*
+
+1)   a%b = a if a<b
+2)   a%(-b) = a%b but -a&b = -(a%b)
+
+3)   comma doesn't allow 2 diff values to be printed
+
+*/
+return 0;
+}
